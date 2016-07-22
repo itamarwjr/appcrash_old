9221ff36ab0392d9611a6469669b9ba01a3ef0d1
itamarwjr <itamarwjr@gmail.com>
2016-07-22 15:09:31 -0300

    mail

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -18,7 +18,7 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 // Create the email and send the message
 $to = 'contato@appcrash.com.br'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
-$email_body = "Novo contato do site appcrash.com.br.\n\n"."A mensagem é:\n\nNome: $name\n\nE-mail: $email_address\n\nTelefone: $phone\n\nMensagem:\n$message";
+$email_body = "Novo contato do site appcrash.com.br\n"."A mensagem é:\nNome: $name\nE-mail: $email_address\nTelefone: $phone\nMensagem:\n$message";
 $headers = "From: noreply@appcrash.com.br\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address\n";
 $headers .= "Content-type: text/html; charset=UTF-8";
